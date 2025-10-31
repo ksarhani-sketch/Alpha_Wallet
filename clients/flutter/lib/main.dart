774e171e888d8ff9296645c8341bf1a3c70fdeac
ksarhani-sketch <ksarhani@gmail.com>
2025-10-31 08:18:11 +0400

    feat: configure amplify auth for flutter client

--- clients/flutter/lib/main.dart
+++ clients/flutter/lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+
+import 'amplifyconfiguration.dart';
 import 'app/app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  Amplify.logLevel = LogLevel.verbose;
+
+  await Amplify.addPlugin(AmplifyAuthCognito());
+
+  try {
+    await Amplify.configure(amplifyconfig);
+  } catch (e) {
+    debugPrint('Amplify.configure error: $e');
+  }
+
   runApp(const ProviderScope(child: AlphaWalletApp()));
 }
