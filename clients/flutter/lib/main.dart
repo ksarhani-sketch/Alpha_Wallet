849150510a3ec05657834ede166e4743fa6b69a9
ksarhani-sketch <ksarhani@gmail.com>
2025-11-01 09:13:31 +0400

    Update main.dart

--- clients/flutter/lib/main.dart
+++ clients/flutter/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_core/amplify_core.dart'; // <- for LogLevel / setLogLevel
 
 import 'amplifyconfiguration.dart';
 import 'app/app.dart';
@@ -10,8 +11,10 @@ import 'app/app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  Amplify.logLevel = LogLevel.verbose;
+  // Amplify v2: use setLogLevel instead of the removed Amplify.logLevel setter
+  Amplify.setLogLevel(LogLevel.verbose);
 
+  // No generic type arg needed in v2
   await Amplify.addPlugin(AmplifyAuthCognito());
 
   try {
